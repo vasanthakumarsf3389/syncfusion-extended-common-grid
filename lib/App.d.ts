e5523a687485b1de95ce45c20d759cf15e8c741d
vasanthakumarsf3389 <vasanthakumar.k@syncfusion.com>
2025-02-18 11:53:31 +0530

    removed unwanted files.

--- lib/App.d.ts
+++
@@ -1,2 +0,0 @@
-declare function Default(): import("react/jsx-runtime").JSX.Element;
-export default Default;
--- lib/data.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const orderData: any;
-export declare const customerData: {
-    CustomerID: string;
-    ContactName: string;
-    CompanyName: string;
-    Address: string;
-    Country: string;
-}[];
-export declare const data: any;
